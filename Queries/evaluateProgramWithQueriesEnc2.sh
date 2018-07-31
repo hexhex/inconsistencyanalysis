70b95d4fe697d7fe577665ad9d3a613e3f1068d9
Christoph Redl <redl@kr.tuwien.ac.at>
2018-07-31 20:55:17 +0200

    small fix

--- Queries/evaluateProgramWithQueriesEnc2.sh
+++ Queries/evaluateProgramWithQueriesEnc2.sh
@@ -6,5 +6,5 @@ $mydir/inlineQueries.sh $1 > $tmpfile
 cat $mydir/meta.encoding.2 > p
 cat $tmpfile >> p
 #cat $mydir/meta.encoding $tmpfile
-dlvhex2 ${@:2} $mydir/meta.encoding $tmpfile
+dlvhex2 ${@:2} $mydir/meta.encoding.2 $tmpfile
 rm $tmpfile
