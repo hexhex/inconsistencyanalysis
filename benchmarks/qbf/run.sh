705188af591befb2e76b3ff11c9793d5691c7841
Christoph Redl <redl@kr.tuwien.ac.at>
2018-09-11 12:56:29 +0200

    small fixes

---
+++ benchmarks/qbf/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+runheader=$(which run_header.sh)
+if [[ $runheader == "" ]] || [ $(cat $runheader | grep "run_header.sh Version 1." | wc -l) == 0 ]; then
+        echo "Could not find run_header.sh (version 1.x); make sure that the benchmark scripts directory is in your PATH"
+        exit 1
+fi
+source $runheader
+
+# run instances
+if [[ $all -eq 1 ]]; then
+	# run all instances using the benchmark script run insts
+	$bmscripts/runinsts.sh "instances/*.inst" "$mydir/run.sh" "$mydir" "$to" "" "" "$req"
+else
+	mydir=$(dirname $0)
+	confstr="dlvhex2 qbfPlain.hex $instance;dlvhex2 --plugindir=$mydir/../../../core/testsuite qbfExt.hex $instance;$mydir/../../Queries/evaluateProgramWithQueries_EiterPolleres06.sh qbfQueries.hex $instance;$mydir/../../Queries/evaluateProgramWithQueries_Redl18.sh qbfQueries.hex $instance"
+
+	$bmscripts/runconfigs.sh "CONF" "$confstr" "$instance" "$to" ""
+fi
+
