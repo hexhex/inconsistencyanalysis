005f4e1a658fc89d9567aba8efb72ec271dd0b94
Christoph Redl <redl@kr.tuwien.ac.at>
2018-09-11 14:57:30 +0200

    small fixes

--- Queries/evaluateProgramWithQueries_Redl18.sh
+++ Queries/evaluateProgramWithQueries_Redl18.sh
@@ -3,8 +3,8 @@
 mydir=$(dirname $0)
 tmpfile=$(mktemp)
 $mydir/inlineQueries.sh $1 > $tmpfile
-cat $mydir/meta.encoding.redl18 > p
-cat $tmpfile >> p
+#cat $mydir/meta.encoding.redl18 > p
+#cat $tmpfile >> p
 #cat $mydir/meta.encoding $tmpfile
-dlvhex2 ${@:2} $mydir/meta.encoding.2 $tmpfile
+dlvhex2 ${@:2} $mydir/meta.encoding.redl18 $tmpfile
 rm $tmpfile
