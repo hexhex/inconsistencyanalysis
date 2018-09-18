6d73a28bd7c53a312c6d8f734604001047ad6131
Christoph Redl <redl@kr.tuwien.ac.at>
2018-09-18 11:54:04 +0200

    add original encoding

--- Queries/evaluateProgramWithQueries_EiterPolleres06orig.sh
+++ Queries/evaluateProgramWithQueries_EiterPolleres06orig.sh
@@ -3,5 +3,5 @@
 mydir=$(dirname $0)
 tmpfile=$(mktemp)
 $mydir/inlineQueries.sh $1 > $tmpfile
-dlvhex2 ${@:2} $mydir/meta.encoding.redl18 $tmpfile
+dlvhex2 ${@:2} $mydir/meta.encoding.eiterpolleres06orig $tmpfile
 rm $tmpfile
