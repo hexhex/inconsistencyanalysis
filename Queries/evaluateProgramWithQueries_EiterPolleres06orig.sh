8fb6f7d39e70134d3cf9dcfb6fe77851f8fc9778
Christoph Redl <redl@kr.tuwien.ac.at>
2018-09-18 20:54:35 +0200

    cleanup

--- Queries/evaluateProgramWithQueries_EiterPolleres06orig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mydir=$(dirname $0)
-tmpfile=$(mktemp)
-$mydir/inlineQueries.sh $1 > $tmpfile
-dlvhex2 ${@:2} $mydir/meta.encoding.eiterpolleres06orig $tmpfile
-rm $tmpfile
--- Queries/evaluateProgramWithQueries_Redl18.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mydir=$(dirname $0)
-tmpfile=$(mktemp)
-$mydir/inlineQueries.sh $1 > $tmpfile
-dlvhex2 ${@:2} $mydir/meta.encoding.redl18 $tmpfile
-rm $tmpfile
