ab10b3ec1e31fc4ade661bf66d0df81bd3f18052
Christoph Redl <redl@kr.tuwien.ac.at>
2018-09-19 23:30:47 +0200

    add TPLP 06 encoding with mod+dep optimizations

--- Queries/evaluate.sh
+++ Queries/evaluate.sh
@@ -3,7 +3,11 @@
 if [[ "$1" = "-h" ]] | [[ $# -lt 3 ]]; then
 	echo -e "Parameters:
   \$1: Solver: dlvhex2 or potassco (ground and solve), or potasscoground (ground only)
-  \$2: Encoding: eiterpolleres06orig, eiterpolleres06 or redl18
+  \$2: Encoding:
+         eiterpolleres06unopt (TPLP 06 \"meta\")
+         eiterpolleres06 (TPLP 06 mod+dep)
+         eiterpolleres06revised (TPLP 06 mod+dep, newly encoded with modern language features)
+         redl18 (new encoding)
   \$3: Main program
   \$4: Instance file(s)
   \$5ff: Additional parameters to the solver"
@@ -19,14 +23,18 @@ fi
 mydir=$(dirname $0)
 inlinedprogram=$(mktemp)
 
-if [[ "$encoding" = "eiterpolleres06" ]]; then
+if [[ "$encoding" = "eiterpolleres06unopt" ]]; then
 	# inline query atoms in the main file
 	$mydir/inlineQueries.sh $mainprog > $inlinedprogram
-	encodingfile="$mydir/meta.encoding.eiterpolleres06"
-elif [[ "$encoding" = "eiterpolleres06orig" ]]; then
+	encodingfile="$mydir/meta.encoding.eiterpolleres06unopt"
+elif [[ "$encoding" = "eiterpolleres06" ]]; then
         # inline query atoms in the main file
         $mydir/inlineQueries.sh $mainprog > $inlinedprogram
-        encodingfile="$mydir/meta.encoding.eiterpolleres06orig"
+        encodingfile="$mydir/meta.encoding.eiterpolleres06"
+elif [[ "$encoding" = "eiterpolleres06revised" ]]; then
+        # inline query atoms in the main file
+        $mydir/inlineQueries.sh $mainprog > $inlinedprogram
+        encodingfile="$mydir/meta.encoding.eiterpolleres06revised"
 elif [[ "$encoding" = "redl18" ]]; then
 	# inline query atoms in the main file
 	$mydir/inlineQueries.sh $mainprog > $inlinedprogram
